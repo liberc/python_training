ba00dac8c818f22d77a3affff8dbe4a0a86273b5
libercat <libercatty@gmail.com>
2019-03-24 14:58:34 +0400

    выделили вспомомгательный класс SessionHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -17,14 +17,14 @@ def app(request):
     return fixture
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="test_name", header="test_header", footer="test_footer"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 # def is_element_present(self, how, what):
 #     try: self.wd.find_element(by=how, value=what)
