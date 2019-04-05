a5c566e1240b41d68bd521ef269c7c56bd5a5ca5
libercat <libercatty@gmail.com>
2019-04-05 07:36:56 +0400

    повысили интеллектуальность функции login и logout

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,9 +2,7 @@ from model.contact import Contact
 
 #контейнер
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.init_creation()
     app.contact.fill_main(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Ivashka", photopath ="C:\\lava.jpg", title="test_title", company="test_company_name", address="Moscow, Sovetskaja str, 100", homephone="+7{495}12345678", mobilephone="891612345678", workphone="891677788899", faxphone="891655544111", email1="testemail1@maill.ru", email2="testemail2@maill.ru", email3="testemail3@maill.ru", homepage="www.homepage.ru", dates_bday="1", dates_bmonth="March", dates_byear="1999", dates_aday="8", dates_amonth="July", dates_ayear="2005", secondary_address="Moscow, Street Test", secondary_home="21", secondary_notes="this is test notes"))
     app.contact.submit_contact_form()
-    app.session.logout()
 
--- test/test_dell_group.py
+++ test/test_dell_group.py
@@ -1,6 +1,4 @@
 
 
 def test_dell_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.delete_first()
-    app.session.logout()
